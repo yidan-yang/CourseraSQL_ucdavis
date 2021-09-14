373f7a207f16e97dd3eac6d3c3a877e784c7fed5
Yidan Yang <yancxxcat@gmail.com>
2021-09-14 13:15:15 -0400

    city names with vowels

--- HackerRankNotes/WeatherObservationStation.sql
+++ HackerRankNotes/WeatherObservationStation.sql
@@ -16,11 +16,38 @@
 --|----------|--------------|
 
 
-----------------------------------
+-------------------------------------------------------------------------------------------
 -- #1 Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) 
 --    as both their first and last characters.  Your result cannot contain duplicates.
-
+-------------------------------------------------------------------------------------------
 select distinct(CITY)
 from STATION
 where left(CITY,1) in ('a','e','i','o','u') and right(CITY,1) in ('a','e','i','o','u')
+
+
+-------------------------------------------------------------------------------------------
+-- #2 Query the list of CITY names from STATION that do not start with vowels. 
+--    Your result cannot contain duplicates.
+-------------------------------------------------------------------------------------------
+select distinct(CITY)
+from STATION
+where CITY REGEXP '^[^aeiou]'
+
+
+-------------------------------------------------------------------------------------------
+-- #3 Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+-- Your result cannot contain duplicates.
+-------------------------------------------------------------------------------------------
+select distinct(CITY)
+from STATION
+where CITY REGEXP '^[aeiou]'
+
+
+-------------------------------------------------------------------------------------------
+-- #4 Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+--    Your result cannot contain duplicates.
+-------------------------------------------------------------------------------------------
+select distinct(CITY)
+from STATION
+where RIGHT(CITY,1) in ('a','e','i','o','u');
 
