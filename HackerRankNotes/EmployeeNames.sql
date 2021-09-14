e8424b21a528de449347491ccca24ef78b045e9c
Yidan Yang <yancxxcat@gmail.com>
2021-09-14 15:29:23 -0400

    Create EmployeeNames.sql

---
+++ HackerRankNotes/EmployeeNames.sql
@@ -0,0 +1,23 @@
+-- The Employee table containing employee data for a company is described as follows:
+----------------------------
+--| Column      |  Type    |
+--|-------------|----------|
+--| employee_id |  Integer |
+--|-------------|----------|
+--| Name        |  String  |
+--|-------------|----------|
+--| Month       |  Integer |
+--|-------------|----------|
+--| Salary      |  Integer |
+--|-------------|----------|
+
+-- employee_id is an employee's ID number
+-- name is their name
+-- months is the total number of months they've been working for the company
+-- salary is their monthly salary
+
+-----------------------------------------------------------------------------------
+-- #1 Write a query that prints a list of employee names 
+--    (i.e.: the name attribute) from the Employee table in alphabetical order.
+-----------------------------------------------------------------------------------
+
